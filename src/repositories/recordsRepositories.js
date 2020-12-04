c16099e1b7ca9b24550c00b887608a70f0fde624
Gabriel Barros <barros.fontes@unesp.br>
2020-12-04 02:14:36 -0300

    fixing locale money issue

--- src/repositories/recordsRepositories.js
+++ src/repositories/recordsRepositories.js
@@ -4,6 +4,8 @@ const dayjs = require('dayjs');
 async function createRecord(record, userId) {
     const { transaction, type, description } = record;
     const date = dayjs(Date.now()).locale('pt-br').format();
+
+    await pool.query('SET LC_MONETARY TO "pt_BR.utf8"');
     
     const querryString = 'INSERT INTO records (transaction, type, description, date, "userId") VALUES ($1,$2,$3,$4,$5) RETURNING *'
     const insertRecord = await pool.query(querryString,[ transaction.replace('.',','), type, description, date, userId ]);
@@ -12,6 +14,8 @@ async function createRecord(record, userId) {
 }
 
 async function findRecordsByUserId(id) {
+    await pool.query('SET LC_MONETARY TO "pt_BR.utf8"');
+
     const records = await pool.query('SELECT * FROM records WHERE "userId" = $1', [ id ]);
     return records.rows;
 }
